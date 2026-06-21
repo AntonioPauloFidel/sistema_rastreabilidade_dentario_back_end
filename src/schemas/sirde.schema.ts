a85f1fe95447ff2ef125b564817ed1844c3fafdd
Antonio Paulo Pereira Fidel <antoniopfdl.fidel@gmail.com>
2026-06-20 21:03:13 -0300

    Revert "Prazo de uso e listagem de processos vencidos"

--- src/schemas/sirde.schema.ts
+++ src/schemas/sirde.schema.ts
@@ -130,7 +130,6 @@ export const cessaoSchema = z.object({
   solicitacaoId: z.string().uuid(),
   instituicaoId: z.string().uuid(),
   denteId: z.string().uuid(),
-  dataLimiteUso: z.coerce.date().optional(),
   observacao: optionalText
 });
 
